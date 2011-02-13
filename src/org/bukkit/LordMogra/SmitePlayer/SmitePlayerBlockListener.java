acd9031b004bd5807df5497b3fa537c23472aa9c
Chris Wrinn <c.wrinn@nixgeex.org>
2011-02-13 00:43:58 -0600

    Clean up!

---
+++ src/org/bukkit/LordMogra/SmitePlayer/SmitePlayerBlockListener.java
@@ -0,0 +1,22 @@
+package org.bukkit.LordMogra.SmitePlayer;
+
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.Material;
+import org.bukkit.event.block.BlockCanBuildEvent;
+import org.bukkit.event.block.BlockListener;
+import org.bukkit.event.block.BlockPhysicsEvent;
+
+/**
+ * SmitePlayer block listener
+ * @author LordMogra
+ */
+public class SmitePlayerBlockListener extends BlockListener {
+    private final SmitePlayer plugin;
+
+    public SmitePlayerBlockListener(final SmitePlayer plugin) {
+        this.plugin = plugin;
+    }
+
+    //put all Block related code here
+}
