d939f62bba951084cbccf29c35f9b5a80232a5fa
Chris Wrinn <c.wrinn@nixgeex.org>
2011-02-12 22:52:23 -0600

    first step to smiting is the first commit

---
+++ src/LordMogra/SmitePlayer/SmitePlayerPlayerListener.java
@@ -0,0 +1,23 @@
+package LordMogra.SmitePlayer;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerEvent;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+
+/**
+ * Handle events for all Player related events
+ * @author LordMogra
+ */
+public class SmitePlayerPlayerListener extends PlayerListener {
+    private final SmitePlayer plugin;
+
+    public SmitePlayerPlayerListener(SmitePlayer instance) {
+        plugin = instance;
+    }
+
+    //Insert Player related code here
+}
+
